0931fb5d81b5714f61952949ba7622815e071b27
Cristian <cristian.segovia.contractor@bbva.com>
2023-03-18 17:46:57 -0500

    P002 : Test to rest

---
+++ src/main/java/com/backend/test/controller/MementoController.java
@@ -0,0 +1,34 @@
+package com.backend.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api-test")
+public class MementoController {
+
+    private List<String> listNames = Arrays.asList("Hola mundo");
+
+    @GetMapping("/hola-mundo")
+    public String getMundo(){
+        return "Hola mundo";
+    }
+
+    @GetMapping("/names")
+    public List<String> getNamesAdd(){
+        return listNames;
+    }
+
+    @PostMapping("/name-add")
+    public ResponseEntity<Map<String,String>> addNames(@RequestParam("name") String name){
+        Map<String,String> mapResponse = new HashMap<>();
+        mapResponse.put("mensaje","Se añadio el nombre ".concat(name));
+        System.out.println(this.listNames);
+        this.listNames.add(name);
+        return ResponseEntity.ok(mapResponse);
+    }
+
+
+}
